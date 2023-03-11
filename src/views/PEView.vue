281770ae72a5ef19743173693c9b7022d3d7ac57
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2023-03-11 13:51:30 +0100

    Add pinia & axios api call example

--- src/views/PEView.vue
+++ src/views/PEView.vue
@@ -1,5 +1,29 @@
 <template>
   <main>
-    PE
+    <h1>Made By Getters</h1>
+    <div v-for="user in getUsers" :key="user.id">
+      {{ user.id }} {{ user.name }} {{ user.address }}
+    </div>
+    <h1>Made By Actions</h1>
+    <div v-for="user in users" :key="user.id">
+      {{ user.id }} {{ user.name }} {{ user.address }}
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import { onMounted, computed } from "vue";
+import { useUserStore } from "../stores/users";
+
+const store = useUserStore();
+const getUsers = computed(() => {
+  return store.getUsers;
+});
+const users = computed(() => {
+  return store.users;
+});
+
+onMounted(() => {
+  store.fetchUsers();
+});
+</script>
