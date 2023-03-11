6fd1abaf95dd73dd9b89aa4c5a180b796b140275
Ferran <ferranfusalba@gmail.com>
2023-03-11 14:06:06 +0100

    Replace dot notation with bracket notation

--- src/views/PEView.vue
+++ src/views/PEView.vue
@@ -1,12 +1,12 @@
 <template>
   <main>
     <h1>Made By Getters</h1>
-    <div v-for="user in getUsers" :key="user.id">
-      {{ user.id }} {{ user.name }} {{ user.address }}
+    <div v-for="user in getUsers" :key="user['id']">
+      {{ user['id'] }} {{ user['name'] }} {{ user['address'] }}
     </div>
     <h1>Made By Actions</h1>
-    <div v-for="user in users" :key="user.id">
-      {{ user.id }} {{ user.name }} {{ user.address }}
+    <div v-for="user in users" :key="user['id']">
+      {{ user['id'] }} {{ user['name'] }} {{ user['address'] }}
     </div>
   </main>
 </template>
