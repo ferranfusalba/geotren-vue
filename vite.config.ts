cf0cfe02d66031952bf0d35184d0139df0dc8900
Ferran <ferranfusalba@gmail.com>
2023-03-11 12:56:51 +0100

    Add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+        cleanupOutdatedCaches: false
+      },
+      manifest: {
+        name: 'Geotren',
+        short_name: 'geotren',
+        description: 'Geotren',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone'
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
