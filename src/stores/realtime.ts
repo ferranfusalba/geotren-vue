8f34129cc519ca23263f58b36935237cc085fa34
Ferran <ferranfusalba@gmail.com>
2023-03-11 18:58:07 +0100

    Change file names & enhance realtime filter

--- src/stores/realtime.ts
+++ src/stores/realtime.ts
@@ -12,7 +12,7 @@ export const useRealTimeStore = defineStore("realTime", {
     actions: {
       async fetchRealTime() {
         try {
-          const data = await axios.get('https://fgc.opendatasoft.com/api/records/1.0/search/?dataset=posicionament-dels-trens&q=&rows=25&exclude.tipus_unitat=113&exclude.tipus_unitat=114&exclude.tipus_unitat=115&exclude.tipus_unitat=112&exclude.desti=PE&exclude.lin=RL1&exclude.lin=RL2&exclude.lin=L8&exclude.lin=S3&exclude.lin=S9')
+          const data = await axios.get('https://fgc.opendatasoft.com/api/records/1.0/search/?dataset=posicionament-dels-trens&q=&rows=25&refine.estacionat_a=PE&exclude.tipus_unitat=113&exclude.tipus_unitat=114&exclude.tipus_unitat=115&exclude.tipus_unitat=112&exclude.desti=PE&exclude.lin=RL1&exclude.lin=RL2&exclude.lin=L8&exclude.lin=S3&exclude.lin=S9')
             this.realTime = data.data.records;
           }
           catch (error) {
