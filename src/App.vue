f69f732773553c3f505cc5d48c16342df6459873
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2023-03-18 18:26:56 +0100

    Add responsiveness to navbar

--- src/App.vue
+++ src/App.vue
@@ -17,27 +17,39 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped lang="scss">
-:root {
-  footer {
-    position: fixed;
-    bottom: 0;
+footer {
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  background-color: #97d700;
 
-    nav {
-      height: 3.75rem;
-      width: 100vw;
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      background-color: #97d700;
+  nav {
+    height: 3.75rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
 
-      a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        height: 100%;
-        color: #51565c;
-      }
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      height: 100%;
+      color: #51565c;
     }
+  }
+}
+
+@media screen and (min-width: 480px) {
+  footer>nav {
+    width: 30rem;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  footer>nav {
+    width: 100%;
   }
 }
 </style>
