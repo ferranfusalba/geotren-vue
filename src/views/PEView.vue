7e96854819c75634d50b2233c501c4a1bb69a902
Ferran <ferranfusalba@gmail.com>
2023-07-09 10:43:08 +0200

    Add line logos on tables

--- src/views/PEView.vue
+++ src/views/PEView.vue
@@ -1,55 +1,82 @@
 <template>
   <main>
     <h2>Origin PE</h2>
-    <EasyDataTable :headers="realTimePEHeaders" :items="realTimePEFields" :hide-footer="true"
-      table-class-name="customize-table" style="--6c2c1440: 0;" />
+    <EasyDataTable
+      :headers="realTimePEHeaders"
+      :items="realTimePEFields"
+      :hide-footer="true"
+      table-class-name="customize-table"
+      style="--6c2c1440: 0"
+    >
+      <template #item-lin="item">
+        <img
+          :src="`/src/assets/lines/${item.lin}.svg`"
+          alt=""
+          style="height: 22px; display: flex; align-items: center"
+        />
+      </template>
+    </EasyDataTable>
+
     <picture>
-      <embed type="image/png" src="https://geotren.fgc.cat/isic/pe" width="100%">
+      <embed type="image/png" src="https://geotren.fgc.cat/isic/pe" width="100%" />
     </picture>
-    <EasyDataTable :headers="schedulePEHeaders" :items="schedulePETimeFiltered" :sort-by="sortBy" :sort-type="sortType"
-      :rows-per-page="5" table-class-name="customize-table" />
+    <EasyDataTable
+      :headers="schedulePEHeaders"
+      :items="schedulePETimeFiltered"
+      :sort-by="sortBy"
+      :sort-type="sortType"
+      :rows-per-page="5"
+      table-class-name="customize-table"
+    >
+      <template #item-route_short_name="item">
+        <img
+          :src="`/src/assets/lines/${item.route_short_name}.svg`"
+          alt=""
+          style="height: 22px; display: flex; align-items: center"
+        />
+      </template>
+    </EasyDataTable>
   </main>
 </template>
 
 <script setup lang="ts">
 // Vue
-import { onMounted, computed } from "vue";
+import { onMounted, computed } from 'vue'
 // Pinia Store
-import { useRealTimeStore } from "../stores/realtime";
-import { useScheduleStore } from "../stores/schedule";
+import { useRealTimeStore } from '../stores/realtime'
+import { useScheduleStore } from '../stores/schedule'
 // Table
-import type { Header, SortType } from "vue3-easy-data-table";
+import type { Header, SortType } from 'vue3-easy-data-table'
 
-const sortBy = "departure_time";
-const sortType: SortType = "asc";
+const sortBy = 'departure_time'
+const sortType: SortType = 'asc'
 const realTimePEHeaders: Header[] = [
-  { text: "Unit", value: "tipus_unitat" },
-  { text: "Line", value: "lin" },
-  { text: "Destination", value: "desti" }
-];
+  { text: 'Unit', value: 'tipus_unitat' },
+  { text: 'Line', value: 'lin' },
+  { text: 'Destination', value: 'desti' }
+]
 const schedulePEHeaders: Header[] = [
-  { text: "Departure", value: "departure_time", sortable: true },
-  { text: "Route", value: "route_short_name" },
-  { text: "Left", value: "left_str" },
-];
+  { text: 'Departure', value: 'departure_time', sortable: true },
+  { text: 'Route', value: 'route_short_name' },
+  { text: 'Left', value: 'left_str' }
+]
 
-
-const realTimeStore = useRealTimeStore();
+const realTimeStore = useRealTimeStore()
 const realTimePEFields = computed(() => {
-  return realTimeStore.getRealTimePEFields;
-});
+  return realTimeStore.getRealTimePEFields
+})
 
-const scheduleStore = useScheduleStore();
+const scheduleStore = useScheduleStore()
 const schedulePETimeFiltered = computed(() => {
-  return scheduleStore.getSchedulePETimeFiltered;
-});
+  return scheduleStore.getSchedulePETimeFiltered
+})
 
 onMounted(() => {
-  realTimeStore.fetchRealTimePE();
-  scheduleStore.fetchTime();
-  scheduleStore.fetchSchedulePE();
-});
-
+  realTimeStore.fetchRealTimePE()
+  scheduleStore.fetchTime()
+  scheduleStore.fetchRealTime()
+  scheduleStore.fetchSchedulePE()
+})
 </script>
 
 <style scoped lang="scss">
