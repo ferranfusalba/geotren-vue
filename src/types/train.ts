8348bfaa473d2222477d240e7804653ccaab05db
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2023-07-15 16:31:11 +0200

    Create train interface

---
+++ src/types/train.ts
@@ -0,0 +1,22 @@
+export interface Train {
+  arrival_time: string
+  date: string
+  departure_time: string
+  exception_time: number
+  left_str: string
+  route_color: string
+  route_long_name: string
+  route_short_name: string
+  route_text_color: string
+  route_type: number
+  route_url: string
+  shape_id: number
+  stop_id: string
+  stop_lat: number
+  stop_lon: number
+  stop_name: string
+  stop_sequence: number
+  timepoint: number
+  trip_headsign: string
+  wheelchair_boarding: number
+}
