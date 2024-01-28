b6dc0e1061a995ca8723f4563cfd6db9a5b48eb0
Ferran <ferranfusalba@gmail.com>
2024-01-28 20:14:32 +0100

    Add tanstack vue table

--- src/views/QCView.vue
+++ src/views/QCView.vue
@@ -3,6 +3,33 @@
     <picture>
       <embed type="image/png" src="https://geotren.fgc.cat/isic/qc" width="100%" />
     </picture>
+    <div>
+      <table>
+        <thead>
+          <tr v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+            <th v-for="header in headerGroup.headers" :key="header.id" :colSpan="header.colSpan">
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in table.getRowModel().rows" :key="row.id">
+            <td v-for="cell in row.getVisibleCells()" :key="cell.id">
+              <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+              <template v-if="cell.column.id === 'left_str'">
+                <CountdownCell :departure_time="row.original.departure_time"></CountdownCell>
+              </template>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div />
+      <!-- <button @click="rerender">Rerender</button> -->
+    </div>
     <EasyDataTable
       :headers="scheduleQCHeaders"
       :items="scheduleQCTimeFiltered"
@@ -35,6 +62,15 @@ import { onMounted, computed } from 'vue'
 import { useScheduleStore } from '../stores/schedule'
 // Table
 import type { Header, SortType } from 'vue3-easy-data-table'
+// TanStack Vue Table
+import {
+  FlexRender,
+  getCoreRowModel,
+  useVueTable,
+  createColumnHelper,
+  SortingState
+} from '@tanstack/vue-table'
+import { ref } from 'vue'
 // Components
 import CountdownCell from '../components/countdown/CountdownCell.vue'
 // Assets
@@ -46,6 +82,8 @@ import R60Logo from '../components/lines/R60Logo.vue'
 import R63Logo from '../components/lines/R63Logo.vue'
 import S4Logo from '../components/lines/S4Logo.vue'
 import S8Logo from '../components/lines/S8Logo.vue'
+// Types
+import type { ScheduleFieldsLeft } from '@/types/schedule'
 
 const sortBy = 'departure_time'
 const sortType: SortType = 'asc'
@@ -61,6 +99,55 @@ const scheduleStore = useScheduleStore()
 const scheduleQCTimeFiltered = computed(() => {
   return scheduleStore.getScheduleQCTimeFiltered
 })
+
+// Testing TanStack Vue Table
+const columnHelper = createColumnHelper<ScheduleFieldsLeft>()
+
+const columns = [
+  columnHelper.accessor('departure_time', {
+    cell: (info) => info.getValue(),
+    header: () => 'Departure',
+    footer: (props) => props.column.id
+  }),
+  columnHelper.accessor((row) => row.route_short_name, {
+    id: 'route',
+    cell: (info) => info.getValue(),
+    header: () => 'Route',
+    footer: (props) => props.column.id
+  }),
+  columnHelper.accessor('trip_headsign', {
+    header: () => 'Sign',
+    footer: (props) => props.column.id
+  }),
+  {
+    id: 'left_str',
+    accessor: 'departure_time',
+    header: () => 'Left'
+  }
+]
+
+const data = ref(scheduleQCTimeFiltered)
+
+// const rerender = () => {
+//   data.value = scheduleQCTimeFiltered
+// }
+
+const sorting = ref<SortingState>([{ id: 'left_str', desc: false }])
+
+const table = useVueTable({
+  get data() {
+    return data.value
+  },
+  columns,
+  state: {
+    get sorting() {
+      return sorting.value
+    }
+  },
+  getCoreRowModel: getCoreRowModel()
+})
+
+// Ends Test
 
 onMounted(() => {
   scheduleStore.fetchTime()
